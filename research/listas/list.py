7e4aca9139c652616b0ee61eeaddb0988f0dea0c
juanamoresworkout <juanamoresworkout@gmail.com>
2025-09-29 18:55:05 +0200

    Resubir

--- research/listas/list.py
+++ research/listas/list.py
@@ -1,12 +1,29 @@
-def contar_palabras(lista):
-    return len(lista)
+#Para poder declarar List:int bien 
+from typing import List
 
-def devolver_mayor(lista):
-    i = 0 
-    buscar_mayor = 0
+def contar_palabras(lista : List[int]) -> int :
+    return len(lista)
+#Me pasan una lista de enteros y devuelvo el numero mayor que hay 
+def devolver_mayor(lista) -> int:
+    i = 1 
+    buscar_mayor = lista[0]
     
     while i < len(lista) : 
         if lista[i] > buscar_mayor :
             buscar_mayor = lista[i]
         i +=1
     return buscar_mayor 
+
+#Una funcion que me devuelva la posicion del mayor elemento
+
+def devolver_maxima(lista : List[int]) -> int : 
+    i = 1
+    contador_posicion = 0 
+    buscar_mayor = lista[0]
+    while i < len(lista) : 
+        if lista[i] > buscar_mayor : 
+            buscar_mayor = lista [i]
+            contador_posicion = i 
+        i +=1 
+        
+    return  contador_posicion 
--- research/listas/main.py
+++ research/listas/main.py
@@ -1,5 +1,7 @@
 #import list
-from list import contar_palabras,devolver_mayor
+from list import contar_palabras,devolver_mayor,devolver_maxima
+
+
 
 mi_lista = [1, 2, 3, 4, 6]
 print(contar_palabras(mi_lista))
@@ -7,3 +9,4 @@ print(contar_palabras(mi_lista))
 print(devolver_mayor(mi_lista))
 #Una funcion que devuelva el numero más grande 
 
+print(devolver_maxima(mi_lista))
