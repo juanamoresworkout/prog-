591f27bad4cccece1bc89871e6e1dad02d897c3f
juanamoresworkout <juanamoresworkout@gmail.com>
2025-09-29 18:21:31 +0200

    Funcion de lista

--- research/listas/list.py
+++ research/listas/list.py
@@ -2,4 +2,11 @@ def contar_palabras(lista):
     return len(lista)
 
 def devolver_mayor(lista):
-    return max(lista)
+    i = 0 
+    buscar_mayor = 0
+    
+    while i < len(lista) : 
+        if lista[i] > buscar_mayor :
+            buscar_mayor = lista[i]
+        i +=1
+    return buscar_mayor 
--- research/listas/main.py
+++ research/listas/main.py
@@ -3,6 +3,7 @@ from list import contar_palabras,devolver_mayor
 
 mi_lista = [1, 2, 3, 4, 6]
 print(contar_palabras(mi_lista))
+
 print(devolver_mayor(mi_lista))
 #Una funcion que devuelva el numero más grande 
 
