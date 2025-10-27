374e8ce718e8b65f4a615b130f1199595822d0f4
juanamoresworkout <juanamoresworkout@gmail.com>
2025-10-27 15:18:37 +0100

    Metodos deck

--- research/clases/Poker/Deck.py
+++ research/clases/Poker/Deck.py
@@ -2,7 +2,7 @@ from PokerCard import *
 import random
 
 class Deck:
-    def _init_(self): 
+    def __init__(self): 
         self._baraja: list[PokerCard] = []
 
     def _init_deck(self): 
@@ -25,9 +25,41 @@ class Deck:
                 i = random.randint(0,len(self._baraja) -1)
                 j = random.randint(0,len(self._baraja) -1)
                 if i == j:
-                    return 
+                    continue 
                 else:
                     self.swap(i,j)
+    def count_baraja(self) :
+        return len(self._baraja)
+    
+    def get_index_card(self,index : int) -> PokerCard | None :
+        if not isinstance (index, int) :
+            return None
+        if index < 0 or index > len(self._baraja):
+            return None
+        return self._baraja[index]
+    
+    def pick_top_card(self, index: int = 0) -> PokerCard | None:
+        if not isinstance(index, int):
+            return None
+        if len(self._baraja) == 0:
+            return None
+        if index < 0 or index >= len(self._baraja):
+            return None
+        return self._baraja.pop(index)
+    def add_card(self, card: PokerCard) -> bool:
+    
+        if not isinstance(card, PokerCard):
+            return False
+        if not card.is_value():
+            return False
 
+        # Compruebo si la carta esta en la baraja con un bucle que compara la carta que metes con todas.
+        for c in self._baraja:
+            if c.get_number() == card.get_number() and c.get_type() == card.get_type():
+                return False 
+
+        self._baraja.append(card)
+        return True
+    
     def show_deck(self) -> list[str]:
         return [carta.return_card() for carta in self._baraja]
--- research/clases/Poker/mine.py
+++ research/clases/Poker/mine.py
@@ -14,8 +14,20 @@ from Deck import Deck
 
 deck = Deck()           
 deck._init_deck()       
-deck.shuffle()          
+deck.shuffle()         
+print(deck.count_baraja())
 
-for carta in deck.show_deck():
-    print(carta)
+for i in range(deck.count_baraja()):
+    c = deck.get_index_card(i)
+    print(c.return_card())
+
+# Saco la carta de arriba
+carta = deck.pick_top_card()
+print("Carta superior:", carta.return_card())
+
+# Saco una carta concreta ejemplo la numero 10
+cartafuera = deck.pick_top_card(10)
+print("Carta 10:", cartafuera.return_card())
+
+print("Cartas restantes:", deck.count_baraja())
 
